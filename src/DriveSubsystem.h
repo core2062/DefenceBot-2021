7ae677d0ac8ef6a30c0c58543cb833e653891745
unknown <50kite17050buster130@gmail.com>
2020-01-25 16:24:46 -0600

    more changes

--- src/DriveSubsystem.h
+++ src/DriveSubsystem.h
@@ -1,36 +1,48 @@
 #pragma once
 
+//#include "Robot.h"
+#include "ctre/Phoenix.h"
 #include <CORERobotLib.h>
-#include <frc/DoubleSolenoid.h>
+#include "AHRS.h"
+#include "COREFramework/COREScheduler.h"
+
 #include <frc/Compressor.h>
-#include <ctre/Phoenix.h>
-#include <COREFramework/COREScheduler.h>
-#include <COREHardware/COREJoystick.h>
+#include <frc/DoubleSolenoid.h>
+#include "WaypointFollower/WaypointFollower.h"
+#include "COREUtilities/CORETimer.h"
 
 enum class DriveSide{LEFT = 1, RIGHT = 2, BOTH = 3};
 
-using namespace frc;
-
-class DriveSubsystem : public CORESubsystem {
+class DriveSubsystem : public CORESubsystem, public CORETask {
 public:
-    DriveSubsystem();
-    void robotInit() override;
-    void teleopInit() override;
-    void teleop() override;
-    void InitTalons();
-    void ToggleGear();
-    void ResetEncoders(DriveSide whichSide);
-    double GetDistanceInInches(DriveSide whichSide);
-    void SetMotorSpeed(double speedInFraction, DriveSide);
-    void SetMotorSpeed(double leftPercent, double rightPercent);
-    double GetForwardPower();
-    void FillCompressor();
+	DriveSubsystem();
+	void robotInit() override;
+	void teleopInit() override;
+	void teleop() override;
+	void teleopEnd() override;
+	void preLoopTask() override;
 
-    COREConstant<double> driveTurnkP;
+    void disabledTask() override;
+
+	void initTalons();
+	void autonInitTask() override;
+
+	void setMotorSpeed(double speedInFraction, DriveSide whichSide);
+	void setMotorSpeed(double leftPercent, double rightPercent);
+	void fillCompressor();
+	void toggleGear();
+	AdaptivePursuit m_pursuit;
+	COREConstant<double> m_lookAhead, m_driveTurnkP;
+	COREVector path;
+    TalonSRX m_leftMaster, m_rightMaster, m_leftSlave, m_rightSlave;
+
 private:
     COREConstant<double> m_etherAValue, m_etherBValue, m_etherQuickTurnValue, m_ticksPerInch;
-    TalonSRX m_leftMaster, m_rightMaster, m_leftSlave, m_rightSlave;
-    DoubleSolenoid m_leftDriveShifter, m_rightDriveShifter;
-    bool m_highGear;
     Compressor compressor;
+	DoubleSolenoid m_leftDriveShifter;
+	bool m_highGear;
+	double m_wheelbase = 20.8;
+	double m_trackwidth = 25.881;
+	AHRS *m_gyro;
+    int count = 0;
 };
