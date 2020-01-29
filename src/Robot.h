9091bdd753af52fccba736c5ae77aefb15dd3dc8
Nathan Johnson <najohnson446@gmail.com>
2020-01-28 19:46:39 -0600

    Worked on conveyor and intake subsystem

--- src/Robot.h
+++ src/Robot.h
@@ -8,12 +8,14 @@
 #include <LauncherSubsystem.h>
 #include <ClimberSubsystem.h>
 #include <IntakeSubsystem.h>
+#include <ConveyorSubsystem.h>
 
 /* Drive ports */
 #define RIGHT_FRONT_PORT 10
 #define RIGHT_BACK_PORT 11
 #define LEFT_FRONT_PORT 12
 #define LEFT_BACK_PORT 13
+#define INTAKE_PORT 14
 
 /* Solenoids */
 #define LEFT_DRIVE_SHIFTER_PCM 1
@@ -23,6 +25,8 @@
 #define RIGHT_DRIVE_SHIFTER_HIGH_GEAR_PORT 2 
 #define LEFT_DRIVE_SHIFTER_LOW_GEAR_PORT 1
 #define RIGHT_DRIVE_SHIFTER_LOW_GEAR_PORT 3
+#define INTAKE_DOWN_PORT 4
+#define INTAKE_UP_PORT 5
 
 using namespace CORE;
 using namespace std;
@@ -36,4 +40,6 @@ public:
 	void test() override;
 	void testInit() override;
 	DriveSubsystem driveSubsystem;
+	IntakeSubsystem intakeSubsystem;
+	ConveyorSubsystem conveyorSubsystem;
 };
