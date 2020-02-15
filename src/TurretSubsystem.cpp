83d15c878d22b7af356ba5ca1544b206889707c9
CORE Programmers <core2062programmers@googlegroups.com>
2020-02-15 14:22:38 -0600

    Manual turret modifications

--- src/TurretSubsystem.cpp
+++ src/TurretSubsystem.cpp
@@ -24,29 +24,35 @@ void TurretSubsystem::teleopInit() {
 }
 
 void TurretSubsystem::teleop() {
-    corePID.SetDerivativeConstant(m_KD.Get());
-    corePID.SetFeedForwardConstant(m_KF.Get());
-    corePID.SetIntegralConstant(m_KI.Get());
-    corePID.SetProportionalConstant(m_KP.Get());
-    auto table = ntinst.GetTable("limelight");
-    bool hasCenterX = table->GetNumber("tv", 0.0) == 1;
-    double conversion = 4096 / -360; // convert degrees to ticks
-    // calculate center error as a percent output for the motor
-    double centerError = table->GetNumber("tx", 0.0) * conversion;
-    double motorPercent = corePID.Calculate(centerError);
+    double manualInput = -operatorJoystick->GetAxis(CORE::COREJoystick::JoystickAxis::LEFT_STICK_X);
+    bool backButtonPressed = operatorJoystick->GetButton(CORE::COREJoystick::JoystickButton::BACK_BUTTON);
+    double motorPercent = 0;
     bool atLeftStop = m_turret.GetSelectedSensorPosition(0) < (m_startupTurretPosition - 468.0);
     bool atRightStop = m_turret.GetSelectedSensorPosition(0) > (m_startupTurretPosition + 468.0);
-    bool backButtonPressed = operatorJoystick->GetButton(CORE::COREJoystick::JoystickButton::BACK_BUTTON);
-    if (hasCenterX && backButtonPressed && ((!atRightStop && centerError < 0) || (!atLeftStop && centerError > 0))) {
-        SetTurret(motorPercent);
-    } else {
-        double manualInput = 0.5 * operatorJoystick->GetAxis(CORE::COREJoystick::JoystickAxis::LEFT_STICK_X);
-        SetTurret(manualInput);
+
+    if (backButtonPressed) {
+        // auto position turret
+        corePID.SetDerivativeConstant(m_KD.Get());
+        corePID.SetFeedForwardConstant(m_KF.Get());
+        corePID.SetIntegralConstant(m_KI.Get());
+        corePID.SetProportionalConstant(m_KP.Get());
+        auto table = ntinst.GetTable("limelight");
+        bool hasCenterX = table->GetNumber("tv", 0.0) == 1;
+        double conversion = 4096 / -360; // convert degrees to ticks
+        // calculate center error as a percent output for the motor
+        double centerError = table->GetNumber("tx", 0.0) * conversion;
+        if (hasCenterX && backButtonPressed && ((!atRightStop && centerError < 0) || (!atLeftStop && centerError > 0))) {
+            motorPercent = corePID.Calculate(centerError);
+        }
+        SmartDashboard::PutNumber("Center Error", centerError);
+        SmartDashboard::PutBoolean("HasTable", hasCenterX);
+    } else if ((!atRightStop && manualInput < 0) || (!atLeftStop && manualInput > 0)) {
+        // manual turret position
+        motorPercent = 0.1 * manualInput;
     }
+    SetTurret(motorPercent);
 
     SmartDashboard::PutNumber("Turret position", m_turret.GetSelectedSensorPosition(0));
-    SmartDashboard::PutNumber("Center Error", centerError);
-    SmartDashboard::PutBoolean("HasTable", hasCenterX);
     SmartDashboard::PutBoolean("At Left Stop", atLeftStop);
     SmartDashboard::PutBoolean("At Right Stop", atRightStop);
     SmartDashboard::PutBoolean("BACK Button Pressed", operatorJoystick->GetButton(CORE::COREJoystick::JoystickButton::BACK_BUTTON));
